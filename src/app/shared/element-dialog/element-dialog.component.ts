04b086ab5ea435db0fa2dc98a31ae661164c71f7
Gustavo12386 <gucalderaro@hotmail.com>
2024-09-12 18:19:32 -0300

    update

--- src/app/shared/element-dialog/element-dialog.component.ts
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject, model, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatDialogModule, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog'; 
-import { PeriodicElement } from '../../views/home/home.component';
+import { PeriodicElement } from '../../models/PeriodicElement';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input'; 
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -5,42 +5,33 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ElementDialogComponent } from '../../shared/element-dialog/element-dialog.component';
+import { PeriodicElement } from '../../models/PeriodicElement';
+import { PeriodicElementService } from '../../services/periodicElement.service';
 
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [MatTableModule, MatButtonModule, MatIconModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  providers: [PeriodicElementService]
+
 })
 export class HomeComponent {
   @ViewChild(MatTable)
   table!: MatTable<any>
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
-  dataSource = ELEMENT_DATA;
+  dataSource!: PeriodicElement[];
 
-  constructor(public dialog: MatDialog){}
+  constructor(
+    public dialog: MatDialog,
+    public periodicElementService: PeriodicElementService 
+  ){
+    this.periodicElementService.getElements().subscribe((data: PeriodicElement[]) => {
+      this.dataSource = data;
+    })
+  }
 
   openDialog(element: PeriodicElement | null): void{
     const dialogRef = this.dialog.open(ElementDialogComponent , {
@@ -51,6 +42,7 @@ export class HomeComponent {
         weight: null,
         symbol: ''
       } : {
+        id: element.id,
         position: element.position,
         name: element.name,
         weight: element.weight,
@@ -60,12 +52,19 @@ export class HomeComponent {
 
     dialogRef.afterClosed().subscribe(result => {
       if(result !== undefined){
-        if(this.dataSource.map(p => p.position).includes(result.position)){
-          this.dataSource[result.position - 1] = result;
-          this.table.renderRows();
+        if (this.dataSource.map(p => p.id).includes(result.id)) {
+          this.periodicElementService.editElement(result)
+            .subscribe((data: PeriodicElement) => {
+              const index = this.dataSource.findIndex(p => p.id === data.id);
+              this.dataSource[index] = data;
+              this.table.renderRows();
+            });
         } else {
-          this.dataSource.push(result);
-          this.table.renderRows();
+          this.periodicElementService.createElements(result).subscribe((data: PeriodicElement) => {
+            this.dataSource.push(result);
+            this.table.renderRows();
+          })
+          
         }
         
       }     
@@ -77,7 +76,10 @@ export class HomeComponent {
   }
 
   deleteElement(position: number): void {
-      this.dataSource = this.dataSource.filter(p => p.position !== position)
+    this.periodicElementService.deleteElement(position)
+    .subscribe(() => {
+      this.dataSource = this.dataSource.filter(p => p.id !== position);
+    });
   }
 
 
